fead8a62670c4ead55bc076f8fe8cf43c89d524c
jekaplugarev <jeka_plugarev@mail.ru>
2022-06-22 17:26:47 +0800

    настройка базы данных

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import {InjectModel} from "nestjs-typegoose";
+import {ModelType} from "@typegoose/typegoose/lib/types";
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+    ) {}
+    
+    async register(dto: any) {
+        
+    }
+}
