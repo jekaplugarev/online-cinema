11d425c33750925463bf5492b8a7bc739f032226
jekaplugarev <jeka_plugarev@mail.ru>
2022-06-28 22:01:55 +0800

    создание jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,15 +8,24 @@ import { ModelType } from '@typegoose/typegoose/lib/types'
 import { UserModel } from 'src/user/user.model'
 import { AuthDto } from './dto/auth.dto'
 import { compare, genSalt, hash } from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+    private readonly jwtService: JwtService
   ) {}
 
   async login(dto: AuthDto) {
-    return this.validateUser(dto)
+    const user = await this.validateUser(dto)
+
+    const tokens = await this.issueTokenPair(String(user._id))
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    }
   }
 
   async register(dto: AuthDto) {
@@ -32,7 +41,12 @@ export class AuthService {
       password: await hash(dto.password, salt),
     })
 
-    return newUser.save()
+    const tokens = await this.issueTokenPair(String(newUser._id))
+
+    return {
+      user: this.returnUserFields(newUser),
+      ...tokens,
+    }
   }
 
   async validateUser(dto: AuthDto): Promise<UserModel> {
@@ -47,5 +61,25 @@ export class AuthService {
     }
 
     return user
+  }
+
+  async issueTokenPair(userId: string) {
+    const data = { _id: userId }
+    const refreshToken = await this.jwtService.signAsync(data, {
+      expiresIn: '15d',
+    })
+    const accessToken = await this.jwtService.signAsync(data, {
+      expiresIn: '1h',
+    })
+
+    return { refreshToken, accessToken }
+  }
+
+  returnUserFields(user: UserModel) {
+    return {
+      _id: user._id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    }
   }
 }
